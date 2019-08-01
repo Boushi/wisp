63eb759b3debfdf0c3d17256f8fcda415e6cdabc
Boushi <chris.gomez.1993@gmail.com>
2019-07-31 19:06:54 -0700

    Read games from file

--- wisp.py
+++ wisp.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template, request, url_for
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, request, url_for, json
+
+games = []
+
+def create_app():
+    app = Flask(__name__)
+    games_list = json.load(open(os.path.join(app.root_path, 'games.json')))
+    for game in games_list:
+        games.append(game)
+    return app
+
+app = create_app()
 
 @app.route('/')
 def home_page():
-    games = [
-        {
-            "name": "werewolf",
-            "players": 8
-        },
-        {
-            "name": "catan",
-            "players": 5
-        },
-    ]
     return render_template('home.html', games=games)
 
 @app.route('/submit', methods=['POST'])
